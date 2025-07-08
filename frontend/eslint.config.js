c66d33d6c8ebaeb1d542e88b82cc5ae875d8c880
kesesek <126723293+kesesek@users.noreply.github.com>
2025-07-08 16:12:30 +1200

    1

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,9 +2,20 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
+  plugins: ["react", "react-hooks", "import"],
+
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["."],
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      },
+      alias: {
+        map: [["@env", "./env.d.ts"]],
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+      }
+    }
   },
-]);
+}
